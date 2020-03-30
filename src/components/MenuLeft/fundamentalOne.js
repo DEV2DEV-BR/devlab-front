cba4380c75fb32c0cae21e4877cfed6dc97247a3
Daniel <danieldeandradelopes@gmail.com>
2020-03-30 09:31:51 -0300

    Added admin, childishOne, childishTwo, fundamentalOne, fundamentalTwo in Left Menu

---
+++ src/components/MenuLeft/fundamentalOne.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import FontDownloadIcon from '@material-ui/icons/FontDownload';
+import FunctionsIcon from '@material-ui/icons/Functions';
+import PublicIcon from '@material-ui/icons/Public';
+import PanoramaIcon from '@material-ui/icons/Panorama';
+import SportsIcon from '@material-ui/icons/Sports';
+import LanguageIcon from '@material-ui/icons/Language';
+import PetsIcon from '@material-ui/icons/Pets';
+import HistoryIcon from '@material-ui/icons/ClearAll';
+
+
+export default function mainListItems(props) {
+
+    const handleToActivitys = (idSubject, subjects) => {
+        props.props.props.push('/activitys', { idSubject, subjects });
+    };
+
+
+    return (
+        <div>
+            <ListItem button onClick={() => handleToActivitys(6, 'Arte')}>
+                <ListItemIcon>
+                    <PanoramaIcon />
+                </ListItemIcon>
+                <ListItemText primary="Arte" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(5, 'Ciências')}>
+                <ListItemIcon>
+                    <PetsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Ciências" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(7, 'Educação Física')}>
+                <ListItemIcon>
+                    <SportsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Educação Física" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(4, 'Geografia')}>
+                <ListItemIcon>
+                    <PublicIcon />
+                </ListItemIcon>
+                <ListItemText primary="Geografia" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(3, 'História')}>
+                <ListItemIcon>
+                    <HistoryIcon />
+                </ListItemIcon>
+                <ListItemText primary="História" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(8, 'Inglês')}>
+                <ListItemIcon>
+                    <LanguageIcon />
+                </ListItemIcon>
+                <ListItemText primary="Inglês" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(1, 'Língua Portuguesa')}>
+                <ListItemIcon>
+                    <FontDownloadIcon />
+                </ListItemIcon>
+                <ListItemText primary="Lingua Portuguesa" />
+            </ListItem>
+            <ListItem button onClick={() => handleToActivitys(2, 'Matemática')}>
+                <ListItemIcon>
+                    <FunctionsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Matemática" />
+            </ListItem>
+
+
+        </div>
+    )
+};
+
+
